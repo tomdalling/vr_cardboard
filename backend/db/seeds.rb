eebceb8731f1a53bf726e1121e5c3e2693ebe40b
Tom Dalling <tom@tomdalling.com>
2018-09-15 19:56:05 +1000

    Seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create([{
+  title: "High Quality",
+  price: 2000,
+},{
+  title: "Premium",
+  price: 3000,
+}])
